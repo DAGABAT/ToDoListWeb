f1568c29d82a775ce6c1448bd11e51b3daddfa60
PredaSys <i.m.diegogalvis@gmail.com>
2022-09-16 14:04:07 -0500

    Aula1 completa

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento)=>{
+  const list = document.querySelector('[data-list]');
+  //Llamando la funcion createTask
+  const task = createTask(evento);
+  list.appendChild(task);
+
+}
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
   const value = input.value;
-  const list = document.querySelector('[data-list]');
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
@@ -20,11 +30,16 @@ const createTask = (evento) => {
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
+  //Creando la variable para almacenar la fecha, en span
+  const dateElement = document.createElement('span');
+  //Adicionando el valor a la variable span
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  //Vinculando el nuevo span a lista hija
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
