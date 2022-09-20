9ba300beda04186273b02f24d24ea8689e427431
PredaSys <i.m.diegogalvis@gmail.com>
2022-09-20 14:51:38 -0500

    clase de completando tareas lista, se tiene funcionando el cuadro de check, de forma persistente, aun despues de actualizar la pagina

--- components/addTask.js
+++ components/addTask.js
@@ -1,69 +1,86 @@
+import { uniqueDates } from '../services/date.js';
 import { checkComplete } from './checkComplete.js';
 import { deleteIcon } from './deleteIcon.js';
 import { readTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
-  //Desactivando el default
-  evento.preventDefault();
-  //Vinculando los formularios de la pagina
-  const list = document.querySelector('[data-list]');
-  const input = document.querySelector('[data-form-input]');
-  const calendar = document.querySelector('[data-form-date]');
-  //Guardando los valores de los formularios
-  const value = input.value;
-  const date = calendar.value;
-  const dateFormat = moment(date).format('DD/MM/YYYY');
+      //Desactivando el default
+      evento.preventDefault();
+      //Vinculando los formularios de la pagina
+      const list = document.querySelector('[data-list]');
+      const input = document.querySelector('[data-form-input]');
+      const calendar = document.querySelector('[data-form-date]');
+      //Guardando los valores de los formularios
+      const value = input.value;
+      const date = calendar.value;
+      const dateFormat = moment(date).format('DD/MM/YYYY');
 
-      if (value == '' ||  date == '' ){
+      if (value == '' || date == '') {
             return
       }
 
-  //Limpiando los formularios
-  input.value = '';
-  calendar.value = '';
-  //Creando objeto para almacenar las variables que queremos
-  const taskObj = {
-    value,
-    dateFormat
-  }
+      //Limpiando los formularios
+      input.value = '';
+      calendar.value = '';
 
-  //vaciando el contenido de la variable list
-  list.innerHTML = '';
+      //Creando la variable complete
+      const complete = false;
 
-  //Creo constante para recuperar lo guardado en memoria
-  const taskList = JSON.parse(localStorage.getItem('task')) || [];
-  //Adiciono a la constante creada los elementos del objeto
-  taskList.push({ value, dateFormat });
+      //Creando objeto para almacenar las variables que queremos
+      const taskObj = {
+            value,
+            dateFormat,
+            complete,
+            id: uuid.v4()
+      }
 
-  //Convirtiendo en string el objeto y subiendolo a la memoria local
-  localStorage.setItem('task', JSON.stringify(taskList));
+      //vaciando el contenido de la variable list
+      list.innerHTML = '';
 
-  readTasks();
+      //Creo constante para recuperar lo guardado en memoria
+      const taskList = JSON.parse(localStorage.getItem('task')) || [];
+      //Adiciono a la constante creada los elementos del objeto
+      taskList.push(taskObj);
+
+      //Convirtiendo en string el objeto y subiendolo a la memoria local
+      localStorage.setItem('task', JSON.stringify(taskList));
+
+      readTasks();
 }
 
 
-export const createTask = ({ value, dateFormat }) => {
+export const createTask = ({ value, dateFormat, complete, id }) => {
 
-  const task = document.createElement('li');
-        task.classList.add('card');
+      const task = document.createElement('li');
+      task.classList.add('card');
 
-  const taskContent = document.createElement('div');
+      const taskContent = document.createElement('div');
 
+      console.log(complete);
 
+      const check = checkComplete(id);
 
-  const titleTask = document.createElement('span');
-        titleTask.classList.add('task');
-        titleTask.innerText = value;
-        taskContent.appendChild(checkComplete());
-        taskContent.appendChild(titleTask);
-  
-  //Creando la variable para almacenar la fecha, en span
-  const dateElement = document.createElement('span');
-  //Adicionando el valor a la variable span
-        dateElement.innerHTML = dateFormat;
-        task.appendChild(taskContent);
-  //Vinculando el nuevo span a lista hija
-        task.appendChild(dateElement);
-        task.appendChild(deleteIcon());
-  return task;
+      //Condicional para evaluar complete
+      if(complete){
+            console.log('completada');
+            check.classList.toggle('fas');
+            check.classList.toggle('completeIcon');
+            check.classList.toggle('far');
+      }
+
+      const titleTask = document.createElement('span');
+      titleTask.classList.add('task');
+      titleTask.innerText = value;
+      taskContent.appendChild(check);
+      taskContent.appendChild(titleTask);
+
+      //Creando la variable para almacenar la fecha, en span
+      const dateElement = document.createElement('span');
+      //Adicionando el valor a la variable span
+      dateElement.innerHTML = dateFormat;
+      task.appendChild(taskContent);
+      //Vinculando el nuevo span a lista hija
+      task.appendChild(dateElement);
+      task.appendChild(deleteIcon());
+      return task;
 };
--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,13 +1,23 @@
-export const checkComplete = () => {
+export const checkComplete = (id) => {
   const i = document.createElement('i');
   i.classList.add('far', 'fa-check-square', 'icon');
-  i.addEventListener('click', completeTask);
+  i.addEventListener('click',(evento) => completeTask(evento,id));
   return i;
 };
 // Immediately invoked function expression IIFE
-const completeTask = (event) => {
+const completeTask = (event, id) => {
   const element = event.target;
   element.classList.toggle('fas');
   element.classList.toggle('completeIcon');
   element.classList.toggle('far');
+  
+  //Obteniendo tasks de localStorage
+  const tasks = JSON.parse(localStorage.getItem('task'));
+  
+  //Identificando el indice de lo clickeado
+  const index = tasks.findIndex( item => item.id == id);
+  console.log(index);
+  tasks[index]['complete'] = !tasks[index]['complete'];
+  console.log(tasks);
+  localStorage.setItem('task',JSON.stringify (tasks));
 };
